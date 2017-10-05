28dd5841b9c16ccc32c72148885b8b904fad7deb
Evandro Harrison Hoffmann <evandro@itsdayoff.com>
2017-10-05 16:12:45 -0300

    Added option to set if is attributed or not. / Added option to choose Localization file.

--- AwesomeLocalization/Classes/Extensions/UIButton+Localized.swift
+++ AwesomeLocalization/Classes/Extensions/UIButton+Localized.swift
@@ -10,32 +10,49 @@ import UIKit
 @IBDesignable
 extension UIButton {
     
-    private static let association = ObjectAssociation<NSObject>()
+    // MARK: - Associations
+    
+    private static let localizedTextAssociation = ObjectAssociation<NSObject>()
+    private static let localizationTableAssociation = ObjectAssociation<NSObject>()
+    private static let isAttributedLocalizationAssociation = ObjectAssociation<NSObject>()
+    
+    // MARK: - Inspectables
     
     @IBInspectable
     public var localizedText: String {
         get {
-            return UIButton.association[self] as? String ?? ""
+            return UIButton.localizedTextAssociation[self] as? String ?? ""
         }
         set (localizedText) {
-            UIButton.association[self] = localizedText as NSObject
+            UIButton.localizedTextAssociation[self] = localizedText as NSObject
             updateLocalization()
         }
     }
     
-    /*@IBInspectable
+    @IBInspectable
     public var localizationTable: String? {
         get {
-            return UIButton.association[self] as? String
+            return UIButton.localizationTableAssociation[self] as? String
         }
         set (localizationTable) {
-            UIButton.association[self] = localizationTable as NSObject?
+            UIButton.localizationTableAssociation[self] = localizationTable as NSObject?
             updateLocalization()
         }
-    }*/
+    }
+    
+    @IBInspectable
+    public var isAttributedLocalization: Bool {
+        get {
+            return UIButton.isAttributedLocalizationAssociation[self] as? Bool ?? false
+        }
+        set (isLocalizationAttributed) {
+            UIButton.isAttributedLocalizationAssociation[self] = isLocalizationAttributed as NSObject
+            updateLocalization()
+        }
+    }
     
     public func updateLocalization() {
-        if let attributedText = localizedText.localizedAttributed(font: titleLabel?.font, fontColor: titleColor(for: .normal)) {
+        if isAttributedLocalization,  let attributedText = localizedText.localizedAttributed(tableName: localizationTable, font: titleLabel?.font, fontColor: titleColor(for: .normal)) {
             setAttributedTitle(attributedText, for: .normal)
         } else {
             setTitle(localizedText.localized(), for: .normal)
--- AwesomeLocalization/Classes/Extensions/UILabel+Localized.swift
+++ AwesomeLocalization/Classes/Extensions/UILabel+Localized.swift
@@ -10,21 +10,49 @@ import UIKit
 @IBDesignable
 extension UILabel {
     
-    private static let association = ObjectAssociation<NSObject>()
+    // MARK: - Associations
+    
+    private static let localizedTextAssociation = ObjectAssociation<NSObject>()
+    private static let localizationTableAssociation = ObjectAssociation<NSObject>()
+    private static let isAttributedLocalizationAssociation = ObjectAssociation<NSObject>()
 
+    // MARK: - Inspectables
+    
     @IBInspectable
     public var localizedText: String {
         get {
-            return UILabel.association[self] as? String ?? ""
+            return UILabel.localizedTextAssociation[self] as? String ?? ""
         }
         set (localizedText) {
-            UILabel.association[self] = localizedText as NSObject
+            UILabel.localizedTextAssociation[self] = localizedText as NSObject
+            updateLocalization()
+        }
+    }
+    
+    @IBInspectable
+    public var localizationTable: String? {
+        get {
+            return UILabel.localizationTableAssociation[self] as? String
+        }
+        set (localizationTable) {
+            UILabel.localizationTableAssociation[self] = localizationTable as NSObject?
+            updateLocalization()
+        }
+    }
+    
+    @IBInspectable
+    public var isAttributedLocalization: Bool {
+        get {
+            return UILabel.isAttributedLocalizationAssociation[self] as? Bool ?? false
+        }
+        set (isLocalizationAttributed) {
+            UILabel.isAttributedLocalizationAssociation[self] = isLocalizationAttributed as NSObject
             updateLocalization()
         }
     }
     
     public func updateLocalization() {
-        if let attributedText = localizedText.localizedAttributed(font: font, fontColor: textColor) {
+        if isAttributedLocalization, let attributedText = localizedText.localizedAttributed(tableName: localizationTable, font: font, fontColor: textColor) {
             self.attributedText = attributedText
         } else {
             text = localizedText.localized()
